e3a6d4d2db9e7b3977cb133722d8fb01da6571fe
Rithish kumar <rithishkumar01092000@gmail.com>
2021-02-19 10:33:57 +0530

    just commit

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 from flask_cors import cross_origin
 from flask import Flask, render_template, request, redirect, url_for
 #from main import text_to_speech
-
-
+import fitz
 from PIL import Image 
 import pytesseract 
 import sys 
@@ -12,9 +11,12 @@ import os
 import random
 import shutil
 
-
+ALLOWED_EXTENSIONS = {'jpg'}
 app = Flask(__name__, static_url_path="/static")
 texts=''
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 def get_text(value):
         
     string = value
@@ -52,17 +54,73 @@ def homepage():
         fp,lp=get_text(pageno)
         print(fp)
         print(lp)
-        if lp==0:
-            lp=fp
+        
         
         if uploaded_file.filename != '':
             filename = uploaded_file.filename
             uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            pages = convert_from_path(path, 500,first_page=fp ,last_page=lp)
+            #pages = convert_from_path(path, 500,first_page=fp ,last_page=lp,poppler)
             image_counter = 1
             global texts
             
+            doc = fitz.open(path)
+            k=1
+            # If user wants to read a single page
+            if lp == 0:
+                page = doc.loadPage(fp-1) #number of page
+                zoom_x = 2.0
+                image_counter+=1
+                zoom_y = 2.0
+                mat = fitz.Matrix(zoom_x,zoom_y)
+                pix = page.getPixmap(matrix=mat)
+                output = os.path.join(app.config['UPLOAD_FOLDER'], "image_to_read.jpg")
+                pix.writeImage(output,"jpeg")
+                
+            # If user wants to read range of pages
+            else:
+                for i in range(fp-1,lp):
+                    page = doc.loadPage(i) #number of page
+                    zoom_x = 2.0
+                    zoom_y = 2.0
+                    image_counter+=1
+                    mat = fitz.Matrix(zoom_x,zoom_y)
+                    pix = page.getPixmap(matrix=mat)
+                    output = os.path.join(app.config['UPLOAD_FOLDER'], "image_"+str(k)+"_to_read.jpg")
+                    pix.writeImage(output,"jpeg")
+                    k+=1
+            mytext = []
+            pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+            #os.remove(path)
+         # Here we load the image(s) created in Text_to_speech folder and read the text in image via pytesseract Optical Character Recognition (OCR) software
+            # thus reading text in images and giving us a string
+            for file in os.listdir(os.path.join(app.config['UPLOAD_FOLDER'])):   
+                if allowed_file(file):
+
+                    data = pytesseract.image_to_string(Image.open(os.path.join(app.config['UPLOAD_FOLDER'],file)))
+                    data = data.replace("|","I") # For some reason the image to text translation would put | instead of the letter I. So we replace | with I
+                    data = data.split('\n')
+                    mytext.append(data)
+
+            
+            # Here we make sure that the text is read correctly and we read it line by line. Because sometimes, text would end abruptly
+            
+            
+            for text in mytext:
+                for line in text:
+                    line = line.strip()
+                    # If line is small, ignore it
+                    if len(line.split(" ")) < 10 and len(line.split(" "))>0:
+                        texts= texts + " " + str(line) + "\n"
+
+                    elif len(line.split(" "))<2:
+                        pass
+                    else:
+                        if line[-1]!=".":
+                            texts = texts + " " + str(line)
+                        else:
+                            texts = texts + " " + line + "\n"
+            '''
             for page in pages:
                 filename='page_'+str(image_counter)+'.jpg'
                 page.save(os.path.join(app.config['UPLOAD_FOLDER'], filename),'jpeg')
@@ -74,7 +132,8 @@ def homepage():
                 text = text.replace('-\n', ' ')
                 texts+=text
             #os.remove(path)
-            shutil.rmtree(app.config['UPLOAD_FOLDER']) 
+            '''
+            #shutil.rmtree(app.config['UPLOAD_FOLDER']) 
              
             
         return redirect(url_for('readingtext',texts=filename))
@@ -85,7 +144,7 @@ def readingtext():
     path=request.args['texts']
     print(texts)
     
-    #shutil.rmtree(app.config['UPLOAD_FOLDER'])  
+    shutil.rmtree(app.config['UPLOAD_FOLDER'])  
     return render_template('upload.html',texts=texts)
 
 if __name__ == "__main__":
